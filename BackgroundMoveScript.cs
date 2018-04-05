4f76bc110364c6cc940b401d9efe3b52b103b165
KevinIRomanNieves <36775789+KevinIRomanNieves@users.noreply.github.com>
2018-04-05 10:20:08 -0400

    Update BackgroundMoveScript.cs
    Este Script cuenta con comentarios  e indentaciones actualizadas para poder utilizar y visualizar mejor la funcionalidad del programa y poder utilizarlo en proximos Niveles del Juego.

--- BackgroundMoveScript.cs
+++ BackgroundMoveScript.cs
@@ -1,34 +1,41 @@
-﻿using System.Collections;
+//Se puede utilizar sistemas de collicion y auto generacion de objetos con esta librerias
+using System.Collections;
 using System.Collections.Generic;
+//Libreria para poder interpresar las palabras predeterminadas por el Engine de Unity
 using UnityEngine;
 
-public class BackgroundMoveScript : MonoBehaviour {
+public class BackgroundMoveScript : MonoBehaviour
+{
 
-	// Background scroll speed can be set in Inspector with slider
+	//Indica un rango de movimiento de 1 a 20f en el cual tendra lugar el movimiento 
+	//Es el Esenario en el cual el background estara moviendose se incrementara por uno
 	[Range(1f, 20f)]
 	public float scrollSpeed = 1f;
 
-	// Scroll offset value to smoothly repeat backgrounds movement
+	
+	//Hace que el valor del scrollOffset se repita 
 	public float scrollOffset;
 
-	// Start position of background movement
+	// Posicion de inicio del movimiento en x del background
 	Vector2 startPos;
 
-	// Backgrounds new position
+	// Nueva posicion de inicio del Backgrounds 
 	float newPos;
 
-	// Use this for initialization
-	void Start () {
-		// Getting backgrounds start position
+	
+	void Start ()
+	{
+		// Hace que el background empiezen el la posicion de inicio
 		startPos = transform.position;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		// Calculating new backgrounds position repeating it depending on scrollOffset
+	// Se llama la funcion por frames
+	void Update () 
+	{
+		// Calcula la nueva posicion del background y lo repite dependiendo del  scrollOffset que tenia un rango de 1f a 20f
 		newPos = Mathf.Repeat (Time.time * - scrollSpeed, scrollOffset);
 
-		// Setting new position
+		// Se encarga de hacer la iteracion para asignar la nueva posicion del background
 		transform.position = startPos + Vector2.right * newPos;
 	}
 }
